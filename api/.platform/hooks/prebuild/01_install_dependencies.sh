df371b8b7f4302c977c8513d1ab2c2be0f07b0ad
이은호 <62738548+gaebal-herolaw@users.noreply.github.com>
2025-07-21 00:19:45 +0900

    api 서버 분리

--- api/.platform/hooks/prebuild/01_install_dependencies.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# 추가 시스템 패키지 설치 (필요한 경우)
-# yum update -y
-# yum install -y gcc python3-devel mysql-devel
-
-echo "Pre-build hook completed successfully"
--- api/application.py
+++
@@ -1,8 +0,0 @@
-# Elastic Beanstalk용 애플리케이션 엔트리 포인트
-from main import app
-
-# EB는 기본적으로 'application' 변수를 찾습니다
-application = app
-
-if __name__ == "__main__":
-    application.run()
